e23084793e6c797ec15b5304e9a95cc92d221cdc
Will Dickson <wbd@caltech.edu>
2022-09-01 12:52:15 -0700

    Renamed Resync macro.

--- src/SyncSpreadsheetAliases.py
+++ src/SyncSpreadsheetAliases.py
@@ -1,18 +1,19 @@
 """
 RedoAliases.py 
 
-Simple macro which resyncs the aliases in a spreadsheet. 
+Simple macro which synchronizes the aliases in a spreadsheet after they
+they've been scrambled due to row/column insertion deletions. 
 
 This is a temporary kludge which I came up with to deal with a bug in the
 current FreeCAD version (0.20) which can cause the aliases to become out of
-sync when inserting/deleting rows and coloumns.  
+sync when inserting/deleting rows and columns.  
 
 """
 import string 
 import FreeCAD
 import xml.etree.ElementTree as ET
 
-__title__    = 'ResyncAliases'
+__title__    = 'SyncSpreadsheetAliases'
 __author__   = 'Will Dickson'
 __version__  = '0.1.01'
 __date__     = '2022-08-30'
@@ -20,7 +21,7 @@ __Comment__  = 'Tempory kludge to deal with spreadsheet row insertion bug'
 __Status__   = 'stable'
 __Requires__ = 'FreeCAD 0.19'
 
-FreeCAD.Console.PrintMessage(f'resyncing aliases\n') 
+FreeCAD.Console.PrintMessage(f'synchronizing spreadsheet aliases\n') 
 
 cells_xml_root = ET.fromstring(App.ActiveDocument.Spreadsheet.cells.Content)
 tmp_suffix = '__tmp__'

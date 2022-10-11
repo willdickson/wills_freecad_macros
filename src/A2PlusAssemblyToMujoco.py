326da848b4a4b5330224e1f26fc43f6745952c63
Will Dickson <wbd@caltech.edu>
2022-10-10 17:31:33 -0700

    Worked on a2plus assemble to mujoco file tool.

--- src/A2PlusAssemblyToMujoco.py
+++ src/A2PlusAssemblyToMujoco.py
@@ -13,7 +13,11 @@ locate, what kind of joints they are etc.
 import os
 import string 
 import FreeCAD
+import xml.dom.minidom
 import xml.etree.ElementTree as ET
+
+from PySide import QtGui
+from PySide import QtCore
 
 __title__    = 'A2PlusAssemblyToMujoco'
 __author__   = 'Will Dickson'
@@ -24,58 +28,144 @@ __Status__   = 'unstable'
 __Requires__ = 'FreeCAD 0.19'
 
 MESH_FILE_DIR = 'mesh_files'
-
-def get_part_objs():
-    """
-    Returns a list of all parts in the assembly document's tree
-    """
-    doc = App.ActiveDocument
-    part_obj_list = [p for p in doc.findObjects() if 'Part:' in p.TypeId]
-    return part_obj_list
+MUJOCO_MODEL_FILE = 'model.xml'
 
 
-def get_part_info(part_obj_list):
+
+def get_part_info():
     """
     Extracts part information, e.g., postion, rotation, source file.
     """
     part_info_dict = {}
-    for part in part_obj_list:
+    for part in App.ActiveDocument.findObjects('Part::FeaturePython'):
         p = part.Placement.Base
         q = part.Placement.Rotation
         axis = part.Placement.Rotation.Axis
         angle = part.Placement.Rotation.Angle
-        src_file = part.sourceFile
+        _, src_file = os.path.split(part.sourceFile)
+        name, _ = os.path.splitext(src_file)
         part_info_dict[part.Label] = {
-                'pos'      : p, 
-                'quat'     : q, 
-                'axis'     : axis,
-                'angle'    : angle,
-                'src_file' : src_file
+                'pos'        : p, 
+                'quat'       : q, 
+                'axis'       : axis,
+                'angle'      : angle,
+                'part_name'  : name, 
+                'part_file'  : src_file,
+                'part_obj'   : part,
                 }
-        #print(part.Label)
-        #for k,v in part_info_dict[part.Label].items():
-        #    print(f'{k}: {v}')
-        #print()
     return part_info_dict
 
 
-def create_mesh_files(part_info_dict):
-    """
-    Creates an stl file for each part object.
+def get_save_info():
+    active_doc_dir, active_doc_file = os.path.split(App.ActiveDocument.FileName)
+    save_dir = select_save_dir_dialog()
+    mesh_dir = os.path.join(save_dir, MESH_FILE_DIR)
+    save_info = {
+            'active_doc_dir'  : active_doc_dir, 
+            'active_doc_file' : active_doc_file, 
+            'save_dir'        : save_dir,
+            'mesh_dir'        : mesh_dir,
+            }
+    return save_info
 
-    Location is where you opened freecad ... 
+
+def select_save_dir_dialog():
     """
-    mesh_dir = os.path.join(os.curdir, MESH_FILE_DIR)
-    os.makedirs(mesh_dir, exist_ok=True)
-    print(mesh_dir)
+    Opens dialog asking for the directory where the mujoco files will be saved.
+    """
+    default_dir = QtCore.QDir.home().path()
+    default_dir = os.path.join(default_dir, 'tmp', 'a2plus_mujoco_test')
+    save_to_dir = QtGui.QFileDialog.getExistingDirectory(
+            parent = None, 
+            caption = 'Select Save Directory', 
+            dir = default_dir, 
+            )
+    return save_to_dir 
+
+
+def get_mesh_file(part_name):
+    mesh_file = f'{part_name}.stl'
+    return mesh_file
+
+
+def create_mesh_files(part_info_dict, save_info):
+    """
+    Creates an stl file for each part in the part_info_dict and saves it to the
+    sub-directory MESH_FILE_DIR of the saveto_dir in the save_info. 
+    """
+    os.makedirs(save_info['mesh_dir'], exist_ok=True)
+    msg = f'saving mesh files to: {mesh_dir}\n'
+    FreeCAD.Console.PrintMessage(msg)
+
+    for item, data in part_info_dict.items():
+        part_file = os.path.join(save_info['active_doc_dir'], data['part_file'])
+        part_name = data['part_name']
+        part_obj = data['part_obj']
+        mesh_dir = save_info['mesh_dir']
+        mesh_file = os.path.join(mesh_dir, get_mesh_file(part_name))
+        FreeCAD.Console.PrintMessage(f'saving:  {mesh_file}\n')
+
+        # Open document and save .stl file
+        FreeCAD.openDocument(part_file)
+        App.setActiveDocument(part_name)
+        doc = App.getDocument(part_name)
+        last_feature_obj = doc.findObjects("PartDesign::Feature")[-1]
+        Mesh.export([last_feature_obj], mesh_file)
+        App.closeDocument(part_name)
+
+
+def create_mujoco_xml_file(part_info_dict, save_info):
+
+    # Create root element 
+    root_elem = ET.Element('mujoco')
+
+    # Add options
+    option_elem_attrib = {
+            'gravity'  : '0 0 -1',
+            'timestep' : '0.005',
+            }
+    option_elem = ET.SubElement(root_elem, 'option', attrib=option_elem_attrib)
+
+    # Add assets
+    asset_elem = ET.SubElement(root_elem, 'asset') 
+    for item, data in part_info_dict.items():
+        part_name = data['part_name']
+        mesh_file = os.path.join('.', MESH_FILE_DIR, get_mesh_file(part_name))
+        mesh_elem_attrib = {'file' : mesh_file}
+        mesh_elem = ET.SubElement(asset_elem, 'mesh', attrib=mesh_elem_attrib)
+
+    # Add colors to assests
+
+    # Save mujoco model to pretty printed xml file
+    xml_str = ET.tostring(root_elem)
+    xml_str = xml.dom.minidom.parseString(xml_str).toprettyxml(indent='  ')
+    save_dir = save_info['save_dir']
+    mujoco_file = os.path.join(save_dir, MUJOCO_MODEL_FILE)
+    with open(mujoco_file, 'w') as f:
+        f.write(xml_str)
+
+
+
+
 
 
 # -----------------------------------------------------------------------------
 
-FreeCAD.Console.PrintMessage(f'Running A2PlusAssemblyToMujoco\n') 
+FreeCAD.Console.PrintMessage('\n')
+msg = 'Running A2PlusAssemblyToMujoco\n'
+FreeCAD.Console.PrintMessage(msg)
 
-part_obj_list = get_part_objs()
-part_info_dict = get_part_info(part_obj_list)
-create_mesh_files(part_info_dict)
+# Get info required for saving files
+save_info = get_save_info()
+
+# Get list of part objects in assembly and extract information
+part_info_dict = get_part_info()
+
+# Create mesh files for all parts in the assembly
+#create_mesh_files(part_info_dict, save_info)
+
+create_mujoco_xml_file(part_info_dict, save_info)
+
+
 
 
